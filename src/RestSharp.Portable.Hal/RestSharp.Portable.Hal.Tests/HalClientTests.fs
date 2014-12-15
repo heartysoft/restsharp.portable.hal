fd61408534484e84da2a2ed6f30f234280fb5133
Tim Stephenson <tim.stephenson391@gmail.com>
2014-12-15 16:57:35 +0000

    initial get working...working on follow

---
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.Tests/HalClientTests.fs
@@ -0,0 +1,44 @@
+﻿module RestSharp.Portable.Hal.Tests
+
+open NUnit.Framework
+open RestSharp.Portable.Hal
+
+let rootUrl = "http://localhost:62582/"
+let clientFactory = 
+            HalClientFactory()
+                .Accept("application/hal+json")
+let client = clientFactory.CreateHalClient(rootUrl)
+
+
+type RegistrationForm = {
+        id:int;
+        name:string
+    }
+
+
+[<TestFixture>]
+type HalTests() = 
+    [<Test>]
+    member test.``should be able to get resource`` () =
+        let resource = client.From("api/cardholders").GetAsync() |> Async.RunSynchronously
+        Assert.IsInstanceOf<Resource>(resource)
+
+    [<Test>]
+    member test.``should be able to get resource following link`` () =
+        let resource = 
+            client.From("api/cardholders")
+                .Follow("register")
+                .GetAsync() |> Async.RunSynchronously
+        
+        let jo = resource.Parse()
+        let tjo = resource.Parse<'RegisterForm>()
+
+        
+        Assert.AreEqual(-1, tjo.id)
+
+    
+    
+
+
+
+    
---
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal/HalClient.fs
@@ -0,0 +1,84 @@
+﻿namespace RestSharp.Portable.Hal
+open RestSharp.Portable
+open Newtonsoft.Json.Linq
+
+type Follow = {rel:string}
+
+type RequestParameters = { rootUrl : string; follow: Follow list }
+
+type EnvironmentParameters = { domain : string; headers : Parameter list }
+
+
+type Resource  = 
+    { requestContext : RequestContext; response : IRestResponse; data: JObject}
+    member this.Parse<'T>() = 
+        this.data.ToObject<'T>()
+and
+    RequestContext = 
+    { environment: EnvironmentParameters; requestParameters : RequestParameters}
+    member private this.parse (response:IRestResponse) : JObject = 
+        let encodingStr = 
+//HOLY KRAP BATMAN...WHY Contains throw exceptionz?!?!?!
+//            match this.response.Headers.Contains("Content-Encoding") with
+//            | true -> 
+//                this.response.Headers.GetValues("Content-Encoding")
+//                |> Seq.head
+//            | _ -> 
+            "UTF-8"
+                         
+        let encoding = System.Text.Encoding.GetEncoding encodingStr
+        let str = encoding.GetString(response.RawBytes, 0, response.RawBytes.Length)
+        
+        JObject.Parse(str)
+
+    member this.GetAsync () : Async<Resource> = 
+        async {
+            let client = RestClient(this.environment.domain)
+            let restRequest = RestRequest(this.requestParameters.rootUrl) :> IRestRequest
+            
+            let req = 
+                this.environment.headers
+                |> List.fold (fun (state:IRestRequest) p -> state.AddParameter(p)) restRequest 
+
+            let! res = client.Execute(req) |> Async.AwaitTask
+            
+            let data = this.parse(res)
+            return { Resource.requestContext = this; response = res; data=data}
+        } 
+    member this.Follow (rel:string) : RequestContext =
+        let rp = this.requestParameters
+        let newRp = {rp with follow = {rel=rel}::rp.follow}
+
+        {this with requestParameters = newRp}
+         
+
+
+type HalClient (env:EnvironmentParameters) = 
+    member this.From (apiRelativeRoot:string) : RequestContext = 
+        {
+            RequestContext.environment = env;
+            requestParameters = {rootUrl = apiRelativeRoot; follow = []}
+        }
+
+
+type HalClientFactory(headers : Parameter list) = 
+    new() = HalClientFactory([])
+    
+    member x.CreateHalClient(domain:string) : HalClient = 
+        HalClient({EnvironmentParameters.domain = domain; headers = headers})
+
+    member x.Header (key:string) (value:string) : HalClientFactory= 
+        let p = new Parameter()
+        p.Name <- key
+        p.Value <- value
+        p.Type <- ParameterType.HttpHeader
+
+        HalClientFactory(p :: headers)
+
+    member x.Accept(headerValue:string) = 
+        x.Header "Accept" headerValue
+
+   
+
+
+
