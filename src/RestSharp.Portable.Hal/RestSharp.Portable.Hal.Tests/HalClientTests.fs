a89e5e9a6c30ea14c9dc40d91948de02dc374e1a
ashic <ashic@live.com>
2014-12-16 03:43:52 +0000

    url expansions working

--- src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.Tests/HalClientTests.fs
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.Tests/HalClientTests.fs
@@ -3,6 +3,8 @@
 open NUnit.Framework
 open RestSharp.Portable.Hal
 
+
+
 let rootUrl = "http://localhost:62582/"
 let clientFactory = 
             HalClientFactory()
@@ -13,6 +15,14 @@ let client = clientFactory.CreateHalClient(rootUrl)
 type RegistrationForm = {
         id:int;
         name:string
+    }
+
+type Card = {idAgain:string}
+
+type CardHolderDetails = {
+        id:int; 
+        name:string;
+        anotherCard:Card
     }
 
 
@@ -42,16 +52,58 @@ type HalTests() =
             client.From("api/cardholders")
                 .Follow("register")
                 .Follow("self")
-                .GetAsync() |> Async.RunSynchronously
+                .GetAsync<RegistrationForm>() |> Async.RunSynchronously
         
-        let jo = resource.Parse()
-        let tjo = resource.Parse<'RegisterForm>()
-
+        Assert.AreEqual(-1, resource.id)
+    
+    [<Test>]
+    member test.``should follow templated link`` () = 
+        let resource = 
+            client.From("api/cardholders")
+                .Follow("cardHolder", ["id" => "123"])
+                .GetAsync<CardHolderDetails>() |> Async.RunSynchronously
         
-        Assert.AreEqual(-1, tjo.id)
+        let expected = {
+            CardHolderDetails.id = 123;
+            name = "Customer Number123";
+            anotherCard = { idAgain = "again" }
+        }
 
-    
-    
+        Assert.AreEqual(expected, resource)
+   
+    [<Test>]
+    member test.``should allow url segment state`` () = 
+        let resource = 
+            client.From("api/cardholders")
+                .UrlSegments(["id" => "123"])
+                .Follow("cardHolder")
+                .GetAsync<CardHolderDetails>() |> Async.RunSynchronously
+        
+        let expected = {
+            CardHolderDetails.id = 123;
+            name = "Customer Number123";
+            anotherCard = { idAgain = "again" }
+        }
+
+        Assert.AreEqual(expected, resource)
+        
+    [<Test>]
+    member test.``provided url segment should take precedence`` () = 
+        let resource = 
+            client.From("api/cardholders")
+                .UrlSegments(["id" => "123"])
+                .Follow("cardHolder", ["id" => "112"])
+                .GetAsync<CardHolderDetails>() |> Async.RunSynchronously
+        
+        let expected = {
+            CardHolderDetails.id = 112;
+            name = "Customer Number112";
+            anotherCard = { idAgain = "again" }
+        }
+
+        Assert.AreEqual(expected, resource)  
+
+    //TODO: Add test for multiple url segments
 
 
 
--- src/RestSharp.Portable.Hal/RestSharp.Portable.Hal/HalClient.fs
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal/HalClient.fs
@@ -2,111 +2,148 @@
 open RestSharp.Portable
 open Newtonsoft.Json.Linq
 
-type Follow = 
-    {rel:string}
-    member this.GetExpandedUrl (rp:RequestParameters) (env:EnvironmentParameters) (data:JObject) : string = 
-        let link = data.["_links"].[this.rel].Value<string>("href")
-        link
-and 
-    RequestParameters = { rootUrl : string; follow: Follow list }
-and
-    EnvironmentParameters = { domain : string; headers : Parameter list }
+[<AutoOpen>]
+module Client =
+  
+    let inline (=>) (left:string) (right) =
+        (left, right.ToString())
 
-
-type Resource  = 
-    { requestContext : RequestContext; response : IRestResponse; data: JObject}
-    member this.Parse<'T>() = 
-        this.data.ToObject<'T>()
-    member this.Follow (next:Follow) rest : RequestContext = 
-        let newRequestParameters = 
-            {this.requestContext.requestParameters 
-                with rootUrl = next.GetExpandedUrl this.requestContext.requestParameters this.requestContext.environment this.data; 
-                     follow=rest }
-        {this.requestContext with requestParameters = newRequestParameters}
-and
-    RequestContext = 
-    { environment: EnvironmentParameters; requestParameters : RequestParameters}
-    member private this.parse (response:IRestResponse) : JObject = 
-        let encodingStr = 
-//HOLY KRAP BATMAN...WHY Contains throw exceptionz?!?!?!
-//            match this.response.Headers.Contains("Content-Encoding") with
-//            | true -> 
-//                this.response.Headers.GetValues("Content-Encoding")
-//                |> Seq.head
-//            | _ -> 
-            "UTF-8"
-                         
-        let encoding = System.Text.Encoding.GetEncoding encodingStr
-        let str = encoding.GetString(response.RawBytes, 0, response.RawBytes.Length)
-        
-        JObject.Parse(str)
-
-    member private this.getResponse () : Async<Resource> = 
-        async {
-            let client = RestClient(this.environment.domain)
-            let restRequest = RestRequest(this.requestParameters.rootUrl) :> IRestRequest
-            
-            let req = 
-                this.environment.headers
-                |> List.fold (fun (state:IRestRequest) p -> state.AddParameter(p)) restRequest 
-
-            let! res = client.Execute(req) |> Async.AwaitTask
-            
-            let data = this.parse(res)
-            return { Resource.requestContext = this; response = res; data=data}
-        } 
-
-    member this.GetAsync () : Async<Resource> = 
-        async {
-            let! rootResponse = this.getResponse()
-            
-            let final = 
-                match this.requestParameters.follow with
-                | [] -> rootResponse
-                | x::xs ->
-                        let newRequest : RequestContext = rootResponse.Follow x xs
-                        newRequest.GetAsync() |> Async.RunSynchronously
-                    
-            return final
-        }
-//        this.requestParameters.follow
-//        |> List.fold (fun (state:Resource) follow -> 
-//                
-//                state) rootResponse
-
-        
-    member this.Follow (rel:string) : RequestContext =
-        let rp = this.requestParameters
-        let newRp = {rp with follow = {rel=rel}::rp.follow}
-
-        {this with requestParameters = newRp}
-         
-
-
-type HalClient (env:EnvironmentParameters) = 
-    member this.From (apiRelativeRoot:string) : RequestContext = 
-        {
-            RequestContext.environment = env;
-            requestParameters = {rootUrl = apiRelativeRoot; follow = []}
-        }
-
-
-type HalClientFactory(headers : Parameter list) = 
-    new() = HalClientFactory([])
+    type Follow = 
+        {rel:string; urlSegments:Parameter list}
+    and 
+        RequestParameters = { rootUrl : string; follow: Follow list; urlSegments : Parameter list }
+    and
+        EnvironmentParameters = { domain : string; headers : Parameter list }
     
-    member x.CreateHalClient(domain:string) : HalClient = 
-        HalClient({EnvironmentParameters.domain = domain; headers = headers})
+    type Resource  = 
+        { requestContext : RequestContext; response : IRestResponse; data: JObject}
+        member this.Parse<'T>() = 
+            this.data.ToObject<'T>()
+        member this.Follow (next:Follow) rest : RequestContext = 
+            let nextUrl = this.data.["_links"].[next.rel].Value<string>("href") 
+            let nextUrlSegments = next.urlSegments
 
-    member x.Header (key:string) (value:string) : HalClientFactory= 
-        let p = new Parameter()
-        p.Name <- key
-        p.Value <- value
-        p.Type <- ParameterType.HttpHeader
+            let newRequestParameters = 
+                {
+                    this.requestContext.requestParameters 
+                        with rootUrl = nextUrl;                          
+                         urlSegments = this.requestContext.requestParameters.urlSegments @ next.urlSegments;
+                         follow=rest 
+                }
 
-        HalClientFactory(p :: headers)
+            {this.requestContext with requestParameters = newRequestParameters}
+    and
+        RequestContext = 
+        { environment: EnvironmentParameters; requestParameters : RequestParameters}
+        member private this.parse (response:IRestResponse) : JObject = 
+            let encodingStr = 
+    //HOLY KRAP BATMAN...WHY Contains throw exceptionz?!?!?!
+    //            match this.response.Headers.Contains("Content-Encoding") with
+    //            | true -> 
+    //                this.response.Headers.GetValues("Content-Encoding")
+    //                |> Seq.head
+    //            | _ -> 
+                "UTF-8"
+                             
+            let encoding = System.Text.Encoding.GetEncoding encodingStr
+            let str = encoding.GetString(response.RawBytes, 0, response.RawBytes.Length)
+            
+            JObject.Parse(str)
 
-    member x.Accept(headerValue:string) = 
-        x.Header "Accept" headerValue
+        member private this.getResponse () : Async<Resource> = 
+            async {
+                let client = RestClient(this.environment.domain)
+                let restRequest = RestRequest(this.requestParameters.rootUrl) :> IRestRequest
+                
+                let parameters = this.environment.headers @ this.requestParameters.urlSegments
+
+                let req = 
+                    parameters
+                    |> List.fold (fun (state:IRestRequest) p -> state.AddParameter(p)) restRequest 
+
+                let! res = client.Execute(req) |> Async.AwaitTask
+                
+                let data = this.parse(res)
+                return { Resource.requestContext = this; response = res; data=data}
+            } 
+
+        member this.GetAsync () : Async<Resource> = 
+            async {
+                let! rootResponse = this.getResponse()
+                
+                let final = 
+                    match this.requestParameters.follow with
+                    | [] -> rootResponse
+                    | x::xs ->
+                            let newRequest : RequestContext = rootResponse.Follow x xs
+                            newRequest.GetAsync() |> Async.RunSynchronously
+                        
+                return final
+            }
+
+        member this.GetAsync<'T> () : Async<'T> = 
+            async{
+                let! response = this.GetAsync()
+                return response.Parse<'T>()
+            }
+          
+        member this.Follow (rel:string, urlSegments: (string*string) list) : RequestContext =
+            let rp = this.requestParameters
+            let segments = 
+                urlSegments 
+                |> List.map (
+                    fun (k, v) -> 
+                        let p = new Parameter()
+                        p.Name <- k
+                        p.Value <- v
+                        p.Type <- ParameterType.UrlSegment
+                        p
+                    )
+
+            let newRp = {rp with follow = rp.follow @ [{rel=rel; urlSegments=segments}]}
+            {this with requestParameters = newRp}
+        
+        member this.Follow (rel:string) : RequestContext =
+            this.Follow (rel, [])
+
+        member this.UrlSegments (urlSegments: (string*string) list) : RequestContext = 
+           let segments = 
+                urlSegments 
+                |> List.map (
+                    fun (k, v) -> 
+                        let p = new Parameter()
+                        p.Name <- k
+                        p.Value <- v
+                        p.Type <- ParameterType.UrlSegment
+                        p
+                    )
+           let newRp = {this.requestParameters with urlSegments = this.requestParameters.urlSegments @ segments}
+           {this with requestParameters = newRp} 
+
+    type HalClient (env:EnvironmentParameters) = 
+        member this.From (apiRelativeRoot:string) : RequestContext = 
+            {
+                RequestContext.environment = env;
+                requestParameters = {rootUrl = apiRelativeRoot; follow = []; urlSegments = []}
+            }
+
+
+    type HalClientFactory(headers : Parameter list) = 
+        new() = HalClientFactory([])
+        
+        member x.CreateHalClient(domain:string) : HalClient = 
+            HalClient({EnvironmentParameters.domain = domain; headers = headers})
+
+        member x.Header (key:string) (value:string) : HalClientFactory= 
+            let p = new Parameter()
+            p.Name <- key
+            p.Value <- value
+            p.Type <- ParameterType.HttpHeader
+
+            HalClientFactory(p :: headers)
+
+        member x.Accept(headerValue:string) = 
+            x.Header "Accept" headerValue
 
    
 
