364f3262f69b8e4b51cd882c431e8c82ad7a97df
ashic <ashic@live.com>
2014-12-17 00:47:20 +0000

    started C# wrapper

---
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.CSharpTests/HalClientTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RestSharp.Portable.Hal.CSharp;
+
+namespace RestSharp.Portable.CSharpTests
+{
+    [TestFixture]
+    public class HalClientTests
+    {
+        [Test]
+        public void should_get_resource()
+        {
+            var clientFactory = new HalClientFactory()
+                .Accept("application/hal+json");
+            var client = clientFactory.CreateHalClient("http://localhost:62582/");
+
+            var resource = client.From("api/cardholders").GetAsync().Result;
+            Assert.IsInstanceOf<Resource>(resource);
+        }
+    }
+}
---
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.CSharp/HalClient.fs
@@ -0,0 +1,32 @@
+﻿namespace RestSharp.Portable.Hal.CSharp
+
+open RestSharp.Portable.Hal
+open System.Collections.Generic
+open System.Threading.Tasks
+
+type Resource internal (inner:Client.Resource) = 
+    class end
+and
+    RequestContext internal (inner:Client.RequestContext) = 
+    member this.GetAsync () = 
+        let work = async {
+            let! result = inner.GetAsync()
+            return Resource(result)
+        }
+        work |> Async.StartAsTask
+
+type HalClient internal (inner:Client.HalClient) = 
+    member this.From domain = 
+        inner.From domain
+        |> fun rc -> RequestContext(rc)
+
+type HalClientFactory private (inner: Client.HalClientFactory) = 
+    new() = HalClientFactory(Client.HalClientFactory())
+
+    member this.Accept x = 
+        inner.Accept x
+        |> fun c -> HalClientFactory(c)
+
+    member this.CreateHalClient domain = 
+        inner.CreateHalClient domain
+        |> fun c -> HalClient(c)
