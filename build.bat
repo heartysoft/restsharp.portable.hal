71baf2bd76a64eae032b7fee07655350ec7dfd92
ashic <ashic@live.com>
2015-01-18 15:36:36 +0000

    passing cmd line arguments through to fake.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 "src\restsharp.portable.hal\.nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "src\restsharp.portable.hal\packages" "-ExcludeVersion"
-"src\restsharp.portable.hal\packages\FAKE\tools\Fake.exe" "build\build.fsx"
+"src\restsharp.portable.hal\packages\FAKE\tools\Fake.exe" "build\build.fsx" %*
 
