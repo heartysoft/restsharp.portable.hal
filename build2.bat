958b5827f13fb230caa27a62df468882eb3eae06
ashic <ashic@live.com>
2015-01-19 04:11:10 +0000

    removed remnants of psake. Fake it is...hopefully ngen will make it faster...

--- build2.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { Import-Module .\tools\psake\psake.psm1; Invoke-psake .\build\build.ps1 %*; exit !($psake.build_success) }" 
--- src/RestSharp.Portable.Hal/Hal.Server/Properties/SolutionInfo.cs
+++ src/RestSharp.Portable.Hal/Hal.Server/Properties/SolutionInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: AssemblyInformationalVersionAttribute("0.0.2-alpha")]
 [assembly: AssemblyCompanyAttribute("Heartysoft Solutions Limited")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Heartysoft Solutions Limited 2015")]
-[assembly: AssemblyMetadataAttribute("githash","master-fd11a0")]
+[assembly: AssemblyMetadataAttribute("githash","master-e8218a")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "0.0.2";
