20e723c1cdb32fc43066c309307f18ba1e7e3907
ashic <ashic@live.com>
2015-01-22 16:56:06 +0000

    bumping nuget

--- src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.CSharp/SolutionInfo.fs
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal.CSharp/SolutionInfo.fs
@@ -1,12 +1,12 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.2-alpha")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.3-alpha")>]
 [<assembly: AssemblyCompanyAttribute("Heartysoft Solutions Limited")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © Heartysoft Solutions Limited 2015")>]
-[<assembly: AssemblyMetadataAttribute("githash","master-e8218a")>]
+[<assembly: AssemblyMetadataAttribute("githash","master-789312")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] Version = "0.0.3"
