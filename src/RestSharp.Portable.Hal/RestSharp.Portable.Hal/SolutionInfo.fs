a5ae971ee4f5d7626a51feef275a9a3320e5571e
ashic <ashic@live.com>
2015-01-23 15:13:34 +0000

    updated restsharp.portable dependency to 2.2.0-pre3 bumped nuget

--- src/RestSharp.Portable.Hal/RestSharp.Portable.Hal/SolutionInfo.fs
+++ src/RestSharp.Portable.Hal/RestSharp.Portable.Hal/SolutionInfo.fs
@@ -1,12 +1,12 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.3-alpha")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.4-alpha")>]
 [<assembly: AssemblyCompanyAttribute("Heartysoft Solutions Limited")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © Heartysoft Solutions Limited 2015")>]
-[<assembly: AssemblyMetadataAttribute("githash","master-789312")>]
+[<assembly: AssemblyMetadataAttribute("githash","master-a27599")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] Version = "0.0.4"
